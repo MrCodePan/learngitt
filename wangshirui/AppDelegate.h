8adefd09c672d4ca968709f1a7129b287bd6ac1d
mac <mac@macdeiMac-203.local.com>
2016-04-25 21:43:53 +0800

    Initial Commit

---
+++ wangshirui/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wangshirui
+//
+//  Created by mac on 16/4/25.
+//  Copyright © 2016年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
